607e44bf2992545638acd775175b239a4c801c1e
Cheburechekmy <pashatarasov@list.ru>
2025-07-11 09:59:01 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
-#include "Student.h"
-#include <iostream>
-
 int main() {
-    try {
-        // Создание объектов
-        Student student1("Иванов Иван", 20, 8.5);
-        Student student2("Петрова Анна", 19, 9.2);
+    // Создание нескольких объектов Student
+    Student student1("Ivan Petrov", 20, 8.5, "Computer Science", "3rd year");
+    Student student2("Anna Sidorova", 19, 7.2, "Economics", "2nd year");
+    Student student3("Petr Ivanov", 21, 5.4, "Mathematics", "4th year");
 
-        // Демонстрация работы
-        student1.displayInfo();
-        student2.displayInfo();
+    // Демонстрация использования объектов
+    cout << "Student information:" << endl;
+    student1.show_info();
+    student2.show_info();
+    student3.show_info();
 
-        // Изменение данных
-        student1.setAverageGrade(9.0);
-        student1.displayInfo();
+    // Изменение данных через сеттеры
+    student1.set_averageScore(9.0);
+    student2.set_age(20);
+    student3.set_faculty("Applied Mathematics");
 
-    } catch (const std::exception& ex) {
-        std::cerr << "Ошибка: " << ex.what() << std::endl;
-    }
+    cout << "\nInformation after changes:" << endl;
+    student1.show_info();
+    student2.show_info();
+    student3.show_info();
 
     return 0;
 }
